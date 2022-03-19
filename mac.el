8ddd3520ec6f095f4788f41aa6012d923df78c2f
fkrause98 <fkrausear@gmail.com>
2022-03-19 12:00:50 -0300

    Warn for missing elixir-ls only in Mac

--- mac.el
+++ mac.el
@@ -33,3 +33,7 @@
 ;; Start server if not running
 (unless (boundp 'server-running-p)
     (server-start))
+
+(if (file-exists-p elixir-ls-folder)
+        (add-to-list 'exec-path "~/elixir-ls")
+        (warn (format "Missing Elixir language server folder in: %s" elixir-ls-folder)))
