3f7edc0c182059ee5c82c2fec291c90112adfb11
Francisco Krause Arnim <fkrausear@gmail.com>
2023-03-28 12:58:55 -0300

    Add magit with difftastic, back to lsp and vertico

--- init.el
+++ init.el
@@ -22,8 +22,8 @@
        (company)           ; the ultimate code completion backend
        ;; (helm +icons +childframe)              ; the *other* search engine for love and life
        ;; ido               ; the other *other* search engine...
-       (ivy +prescient +icons)  ;a search engine for love and life
-       ;; (vertico +icons)
+       ;; (ivy +prescient +icons)  ;a search engine for love and life
+       (vertico +icons)
 
        :ui
        ;;deft            ;notational velocity for Emacs
@@ -93,7 +93,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       (lsp +peek +eglot)
+       (lsp +peek)
        (magit)             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
